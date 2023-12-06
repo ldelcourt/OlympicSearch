8625273650fc6a858b10ec81cf8781929fcca36e
sarahpgl <100869069+sarahpgl@users.noreply.github.com>
2023-12-06 22:35:07 +0100

    barre recherche renvoie données écrite

--- src/Component/Search.tsx
+++ src/Component/Search.tsx
@@ -23,6 +23,8 @@ function Search() {
      <div className="search-container">
   <input type="text" id="search" value={texteSaisie} onChange={handleChange}  placeholder="Rechercher..."></input>
   <button onClick={handleClick} type="submit" id="search-button" >
+  <input type="text" id="search" value={texteSaisie} onChange={handleChange}  placeholder="Rechercher..."></input>
+  <button onClick={handleClick} type="submit" id="search-button" >
     <img src="https://cdn-icons-png.flaticon.com/256/3917/3917132.png" alt="Rechercher"></img>
   </button>
 </div>
