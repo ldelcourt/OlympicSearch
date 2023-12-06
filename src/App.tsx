8fd9f9fa11d948d618a3491cca583fb5e46e58b7
greg <gregoiremuller3@gmail.com>
2023-12-06 17:27:36 +0100

    branche athlete

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import Athlete from "./pages/athlete/athlete";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/athlete/:name" Component={Athlete} />
+
       </Routes>
     </BrowserRouter>
   );
