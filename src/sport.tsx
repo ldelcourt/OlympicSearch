ef0634c0d7c478d8449852e9caca349cf35b5956
JadeLRX <jade.lrx0@gmail.com>
2023-12-06 11:27:55 +0100

    sport page

---
+++ src/sport.tsx
@@ -0,0 +1,32 @@
+import './sport.css';
+
+interface SportProps {
+    sportName: string;
+    olympicAppearance: string;
+    sportImageSource: string;
+    sportDescription: string;
+  }
+
+function Sport({
+    sportName,
+    olympicAppearance,
+    sportImageSource,
+    sportDescription,
+  }: SportProps) {
+    return (
+      <div className="sport-container">
+        <div className="info-container">
+          <div className="info-text">
+            <p>Discipline: {sportName}</p>
+            <p>Apparition aux JO: {olympicAppearance}</p>
+          </div>
+          <img className="sport-image" src={sportImageSource} alt="Sport" />
+        </div>
+        <div className="description-container">
+          <p>{sportDescription}</p>
+        </div>
+      </div>
+    );
+  }
+
+export default Sport;
