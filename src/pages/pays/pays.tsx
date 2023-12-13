5e6842a004c32fc589be421058eb6fcd60bac43d
ldelcourt <lou.delcourt@insa-lyon.fr>
2023-12-13 11:03:42 +0100

    Page pays fonctionnelle

--- src/pages/pays/pays.tsx
+++ src/pages/pays/pays.tsx
@@ -9,9 +9,23 @@ interface QueryMedal {
   totalBronze: { value: string };
   olympicEdition: { value: string };
 }
+
+interface QueryCountry {
+  name: { value: string };
+  image: { value: string };
+}
+
+interface QueryAthlete {
+  name: { value: string };
+  sportif: { value: string };
+}
+
+
+
 function Pays() {
-  const [medalData, setMedalData] = useState<any>([]);
-  const [countryData, setCountryData] = useState<any>([]);
+  const [medalData, setMedalData] = useState<QueryMedal[]>([]);
+  const [countryData, setCountryData] = useState<QueryCountry[]>([]);
+  const [athleteData, setAthleteData] = useState<QueryAthlete[]>([]);
   const { idPays } = useParams();
   const naviguate = useNavigate();
   
@@ -46,8 +60,29 @@ function Pays() {
       ?pays_edition wdt:P17 ?country.
       ?country wdt:P1813 ?name.
       ?country wdt:P41 ?image.
+      FILTER(lang(?name) = 'fr')
     }
-    `
+    `;
+
+    const athleteQuery = `
+    SELECT DISTINCT ?sportif ?name WHERE {
+      ?pays_edition wdt:P179 wd:${idPays}.
+      ?pays_edition wdt:P17 ?country.
+      ?sportif wdt:P1532 ?country.
+      ?sportif p:P1344 ?participation.
+      ?participation ps:P1344 ?competitions.
+      ?competitions rdfs:label ?nameCompetition.
+      ?participation pq:P1352 ?ranking.
+      ?sportif rdfs:label ?name.
+      ?sportif wdt:P569 ?birthdate.
+      FILTER(lang(?nameCompetition) = 'en')
+      FILTER(lang(?name) = 'fr')
+      FILTER(CONTAINS(?nameCompetition, 'Summer'))
+      FILTER(?ranking <= 1)
+      FILTER(?birthdate >= "1970-01-01T00:00:00Z"^^xsd:dateTime)
+    }
+    LIMIT 10
+    `;
 
   const fetchData = async (query:string, setData:React.Dispatch<any>) => {
     const base_endpoint = "https://query.wikidata.org/sparql";
@@ -58,7 +93,7 @@ function Pays() {
 
       if (response.ok) {
         const result = await response.json();
-        setData(result);
+        setData(result.results.bindings);
         console.log(result);
       } else {
         console.error("Erreur lors de la requête SPARQL");
@@ -68,27 +103,52 @@ function Pays() {
     }
   };
 
-  const TableRow: React.FC<{infos: QueryMedal}> = ({ infos }) => (
+  const MedalTableRow: React.FC<{infos: QueryMedal}> = ({ infos }) => (
       <tr>
-        <td onClick={() => {naviguate(`/edition/${infos.olympicEdition.value.substring(infos.olympicEdition.value.lastIndexOf('/') + 1)}`)}}>{infos.label.value.match(/\b\d{4}\b/)}</td>
+        <td className='hoverable' onClick={() => {naviguate(`/edition/${infos.olympicEdition.value.substring(infos.olympicEdition.value.lastIndexOf('/') + 1)}`)}}><a>{infos.label.value.match(/\b\d{4}\b/)}</a></td>
         <td>{infos.totalGold.value}</td>
         <td>{infos.totalSilver.value}</td>
         <td>{infos.totalBronze.value}</td>
       </tr>
   );
-  useEffect(() => { 
+
+  const AthleteRow: React.FC<{data: QueryAthlete}> = ({ data }) => (
+    <div className='hoverable' onClick={() => naviguate(`/athlete/${data.sportif.value.substring(data.sportif.value.lastIndexOf('/') + 1)}`)}>
+      {data?.name?.value}, 
+    </div>
+  );
+
+  useEffect(() => {
     fetchData(medalQuery, setMedalData);
     fetchData(countryQuery, setCountryData);
-    //fetchData(athleteQuery, setAthleteData);
-
-   }, []);
+    fetchData(athleteQuery, setAthleteData);
+  }, []);
+  
+  
+  
 
 
   return (
     <>
       <div className='countryInfo'>
-      <h1>Pays : {countryData?.results?.bindings[0].name.value}</h1>
-      <img className='flag' src={countryData?.results?.bindings[0].image.value} alt="Drapeau" />
+          <div className='textCountry'>
+            <p>Pays : {countryData[0]?.name.value}</p>
+            <p>Nombre de participations aux JOs: {medalData?.length}</p>
+            <div style={{ display: 'flex', flexDirection: 'row'}}>Athlètes notables: 
+            {
+              athleteData.length > 0 ? (
+                athleteData?.map((athlete, index) => (
+                  <AthleteRow key={index} data={athlete} />
+                ))
+              ) : (
+                <div> Aucun</div>
+              )
+            }
+            </div>
+          </div>
+        <div>
+          <img className='flag' src={countryData[0]?.image.value} alt="Drapeau" />
+        </div>
       </div>
       <table>
         <thead>
@@ -97,19 +157,19 @@ function Pays() {
               Année
             </th>
             <th>
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Gold_medal_olympic.svg/330px-Gold_medal_olympic.svg.png" alt="Médaille d'Or"/>
+              <img className='medal' src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Gold_medal_olympic.svg/330px-Gold_medal_olympic.svg.png" alt="Médaille d'Or"/>
             </th>
             <th>
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Silver_medal_olympic.svg/330px-Silver_medal_olympic.svg.png" alt="Médaille d'Argent"/>
+              <img className='medal' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Silver_medal_olympic.svg/330px-Silver_medal_olympic.svg.png" alt="Médaille d'Argent"/>
             </th>
             <th>
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Bronze_medal_olympic.svg/1920px-Bronze_medal_olympic.svg.png" alt="Médaille de Bronze"/>
+              <img className='medal' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/Bronze_medal_olympic.svg/1920px-Bronze_medal_olympic.svg.png" alt="Médaille de Bronze"/>
             </th>
           </tr>
         </thead>
         <tbody>
-          {medalData?.results?.bindings.map((row:QueryMedal, index) => (
-            <TableRow key={index} infos={row} />
+          {medalData?.map((row:QueryMedal, index) => (
+            <MedalTableRow key={index} infos={row} />
           ))}
         </tbody>
       </table>
